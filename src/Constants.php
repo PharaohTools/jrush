47bfce987e15805ef91d74a0eb43410579e5301e
Pharaoh Tools <phpengine@hotmail.co.uk>
2015-12-30 08:10:09 +0000

    new constants

--- src/Constants.php
+++ src/Constants.php
@@ -4,9 +4,38 @@
  * Pharaoh Tools Constants
  */
 
+define('PHARAOH_APP', "ptconfigure") ;
+
 if (in_array(PHP_OS, array("Windows", "WINNT"))) {
+    $sd = getenv('SystemDrive') ;
+    $pf = getenv('ProgramFiles') ;
+    $pf = str_replace(" (x86)", "", $pf) ;
+    $command = "where /R \"{$pf}\" *VBoxManage* " ;
+    $outputArray = array();
+    exec($command, $outputArray);
+    define('SUDOPREFIX', "");
+    define('VBOXMGCOMM', "\"{$outputArray[0]}\" ") ;
+    define('PFILESDIR', $sd."\\PharaohTools\\") ;
+    define('PTCCOMM', PFILESDIR.'ptconfigure.cmd"') ;
+    define('PTBCOMM', PFILESDIR.'ptbuild.cmd"') ;
+    define('PTDCOMM',  PFILESDIR."ptdeploy.cmd") ;
+    define('PTVCOMM',  PFILESDIR."ptvirtualize.cmd") ;
+    define('BOXDIR', $sd.'\\PharaohTools\boxes') ;
+    define('PIPEDIR', $sd.'\\PharaohTools\pipes'.'\\') ;
     define("DS", "\\");
     define("BASE_TEMP_DIR", getenv("SystemDrive").'\Temp\\'); }
 else if (in_array(PHP_OS, array("Linux", "Solaris", "FreeBSD", "OpenBSD", "Darwin"))) {
+    $uname = exec('whoami');
+    $isAdmin = ($uname == "root") ? true : false ;
+    if ($isAdmin == true) { define('SUDOPREFIX', ""); }
+    else { define('SUDOPREFIX', "sudo "); }
+    define('VBOXMGCOMM', "vboxmanage ") ;
+    define('PFILESDIR', "/opt/") ;
+    define('PTCCOMM', "ptconfigure ") ;
+    define('PTBCOMM', "ptbuild ") ;
+    define('PTDCOMM', "ptdeploy ") ;
+    define('PTVCOMM', "ptvirtualize") ;
     define("DS", "/");
-    define("BASE_TEMP_DIR", '/tmp/'); }
+    define("BASE_TEMP_DIR", '/tmp/');
+    define('BOXDIR', '/ptvirtualize/boxes'.'\\') ;
+    define('PIPEDIR', '/opt/ptbuild/pipes') ; }
